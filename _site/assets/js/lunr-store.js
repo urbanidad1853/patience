ea69dce2e6f8d71ffbcbb64cf504f7e61faf2266
urbanidad1853 <p.schell@abdn.ac.uk>
2026-03-26 17:16:24 +0000

    again

--- _site/assets/js/lunr-store.js
+++ _site/assets/js/lunr-store.js
@@ -18,4 +18,40 @@ var store = [{
  
 "id": "español.html#español_p4"
 
+},{  
+"title": "Acta de la declaración Independencia de las Provincias Unidas del Sur en aymara","date": "1816","creator": "Congreso de las Provincias Unidas en Sud-América","description": "Acta de la declaración Independencia de las Provincias Unidas del Sur en aymara","subject": "independencia; provincias unidas del sur","location": "Tucumán", 
+"id": "aymara.html"
+
+},{  
+ 
+"id": "aymara.html#aymara_p1"
+
+},{  
+ 
+"id": "aymara.html#aymara_p2"
+
+},{  
+ 
+"id": "aymara.html#aymara_p3"
+
+},{  
+ 
+"id": "aymara.html#aymara_p4"
+
+},{  
+"title": "Acta de la declaración Independencia de las Provincias Unidas del Sur en quechua","date": "1816","creator": "Congreso de las Provincias Unidas en Sud-América","description": "Acta de la declaración Independencia de las Provincias Unidas del Sur en quechua","subject": "independencia; provincias unidas del sur","location": "Tucumán", 
+"id": "quechua.html"
+
+},{  
+ 
+"id": "quechua.html#quechua_p1"
+
+},{  
+ 
+"id": "quechua.html#quechua_p2"
+
+},{  
+ 
+"id": "quechua.html#quechua_p3"
+
 }];
